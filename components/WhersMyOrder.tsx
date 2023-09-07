0711703e3a4153984ed17e0d990eba95c966c652
mawayalebo <mawayalebo@gmail.com>
2023-09-07 12:36:35 +0200

    changing wheres my order component bg colour when hovering

--- components/WhersMyOrder.tsx
+++ components/WhersMyOrder.tsx
@@ -2,7 +2,7 @@ function WhersMyOrder() {
   return (
    
     <div className="p-2 bg-white shadow-md cursor-pointer h-fit">
-    <div className="relative flex items-center overflow-hidden bg-blue-50 gap-6">
+    <div className="relative flex items-center overflow-hidden bg-blue-50 hover:bg-blue-100 gap-6">
         <div className="absolute bg-[#50bcff] rounded-full w-40 h-40 -left-20 "></div>
         <img src="/images/truck_image.svg" className="objectfit-contain z-10" width={60} height={30}/>
         <div className="flex flex-col p-2 pl-4">
