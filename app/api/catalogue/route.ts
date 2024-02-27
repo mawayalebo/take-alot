3f901bc625febb4de7b4dd194b3c55951e1799be
mawayalebo <mawayalebo@gmail.com>
2024-02-27 14:52:29 +0200

    adding product object properties

--- app/api/catalogue/route.ts
+++ app/api/catalogue/route.ts
@@ -3,7 +3,7 @@ import { NextResponse } from 'next/server';
 
 interface Catalog {
   title: string;
-  products: string[]; // You can use a more structured product interface if needed
+  products: any // You can use a more structured product interface if needed
 }
 
 export async function GET(): Promise<NextResponse> {
@@ -12,46 +12,10 @@ export async function GET(): Promise<NextResponse> {
   return NextResponse.json({ catalog });
 }
 
-// async function scrapeTakealotCatalog(): Promise<Catalog[]> {
-//   const browser = await puppeteer.launch();
-//   const page = await browser.newPage();
-  
-//   try {
-//     // Navigate to takealot.com or specific category page
-//     await page.goto('https://www.takealot.com', { timeout: 1200000 });
-//     const startIndex = 0
-//     const catalog = await page.$$eval('.product-strip-title', (titleElements: any) =>
-//       titleElements.map((titleElement: any) => {
-//         const title = titleElement.querySelector('h2').innerText; 
-//         const products = Array.from(
-//           document.querySelectorAll('.product-card')
-          
-//         ).slice(0,9).map((productElement: any) => {
-//           const productTitle = productElement.querySelector('.card-section a h4.product-title').innerText;
-          
-//           return productTitle;
-          
-//         });
-
-        
-
-//         return {
-//           title,
-//           products,
-//         };
-//       })
-//     );
-
-//     return catalog;
-//   } catch (error) {
-//     console.error('Error while scraping:', error);
-//     return [];
-//   } finally {
-//     await browser.close();
-//   }
-// }
+//incrementing indexing
 let startIndex = 0;
 let endIndex = 10;
+
 async function scrapeTakealotCatalog(startIndex: number, endIndex: number): Promise<Catalog[]> {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
@@ -67,8 +31,26 @@ async function scrapeTakealotCatalog(startIndex: number, endIndex: number): Prom
         const products = Array.from(
           document.querySelectorAll('.product-card')
         ).slice(startIndex, endIndex).map((productElement: any) => {
-          const productTitle = productElement.querySelector('.card-section a h4.product-title').innerText;
-          return productTitle;
+          const productTitle = productElement.querySelector('.card-section a h4.product-title')?.innerText || null;
+          const productThumbLink = productElement.querySelector('.card-section .product-thumb-container .product-thumb-wrapper a div.image-box.product-thumb img')?.getAttribute('src') || '';
+          const now = productElement.querySelector('.card-section ul li.price span')?.innerText || null;
+          const list = productElement.querySelector('.card-section ul li.list-price span')?.innerText || null;
+          const rating = productElement.querySelector('.card-section .rating div')?.innerText || null;
+          const raters = productElement.querySelector('.card-section .rating div span')?.innerText || null;
+          const saleImg = productElement.querySelector('ul.badge-list li div.badge.image.small img')?.getAttribute('src') || null;
+          const salePercentage = productElement.querySelector('ul.badge-list li div.badge.saving.small span')?.innerText || null;
+          return {
+            productTitle,
+            price:{
+              now,
+              list
+            },
+            productThumbLink,
+            rating,
+            raters,
+            saleImg,
+            salePercentage,
+          };
         });
         startIndex += 10
         endIndex += 9
